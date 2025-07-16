35dba29a7fca8e6cc1905c17208ed0a0db714798
Ayush778hacker <dixitayush19d@gmail.com>
2025-07-16 17:16:11 +0530

    4 tak Qusestions complete

--- code.cpp
+++ code.cpp
@@ -9,10 +9,34 @@ int main()
    cout<<"Sum is: "<< a+b<< endl;
 */
 
-//2
+/*2
 
  int a=15;
  int b=16;
  cout<<"Sum of "<<a<<" and "<<b<<" is:"<<a+b<<endl;
+ */
+
+/*3
+    int a;
+    cout<<"Enter First Number: ";
+    cin>>a;
+    int b;
+    cout<<"Enter Second Number: ";
+    cin>>b;
+    cout<<"The Sum of "<<a<<" & "<<b<<" is: "<<a+b<<endl;
+ */
+
+ /*4
+
+    string name;
+    cout<<"Enter Your Name: ";
+    cin>>name;
+
+    int age;
+    cout<<"Enter Your Age: ";
+    cin>>age;
+
+    cout<<"Hello "<<name<<" ,you are "<<age<<" years old."<<endl;
+    */
    return 0;
 }
