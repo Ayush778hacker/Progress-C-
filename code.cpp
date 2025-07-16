85b4b8770491d383cdef3a2c71fec18828826058
Ayush778hacker <dixitayush19d@gmail.com>
2025-07-16 16:59:48 +0530

    2 questions done

---
+++ code.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+/*1
+   int a= 15;
+   int b= 16;
+   cout<<"Sum is: "<< a+b<< endl;
+*/
+
+//2
+
+ int a=15;
+ int b=16;
+ cout<<"Sum of "<<a<<" and "<<b<<" is:"<<a+b<<endl;
+   return 0;
+}
