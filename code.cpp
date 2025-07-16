77f07b085ae6232a2a0e4b47dc717ab6ccb124db
Ayush778hacker <dixitayush19d@gmail.com>
2025-07-16 18:31:33 +0530

    Queston 8 tak

--- code.cpp
+++ code.cpp
@@ -38,5 +38,83 @@ int main()
 
     cout<<"Hello "<<name<<" ,you are "<<age<<" years old."<<endl;
     */
-   return 0;
+
+
+ /*5
+ 
+   int a= 15;
+   int b= 17;
+
+   cout<< --b + a++ + --a + b++;
+   */
+
+   /*6
+   
+   
+   Part--1(Using third variable)*/
+    /*
+   int a = 14;
+   int b = 15;
+
+   int temp = a;
+   a=b;
+   b=temp;
+    cout<<a<<b<<temp;
+    */
+
+   /*Part --2(Without using third variable)
+
+    int a=15;
+    int b=14;
+
+    a=a+b;
+    b=a-b;
+    a=a-b;
+
+    cout<<"Value of a is: "<<a<<endl;
+    cout<<"Value of b is: "<<b<<endl;
+
+    */
+
+    /*7
+    int len;
+    cout<<"Enter alue of Length: ";
+    cin>>len;
+
+    int breadth;
+    cout<<"Enter value of Breath: ";
+    cin>>breadth;
+
+    cout<<"The Area of Reactangle is: "<<len*breadth<<endl;
+    cout<<"The Perimeter of Rectangle is: "<<2*(len+breadth)<<endl;
+    */
+
+    /*8*/
+
+    
+
+    int t;
+    cout<<"No of Years (t): ";
+    cin>>t;
+
+    int P;
+    cout<<"Enter Principal Amount (P): ";
+    cin>>P;
+
+    int r;
+    cout<<"Annual Interest Rate (r): ";
+    cin>>r;
+    int d=r/100;
+     
+
+    int n;
+    cout<<"Number of times interest is compounded per year (n): ";
+    cin>>n;
+
+    int p= n*t;
+    int A=P*(((1+d/n))^p)-P;
+    cout<<"d "<<d<<endl;
+    cout<<"p "<<p<<endl;
+    cout<<"Your Compound Interest is: "<<A<<endl;
+    return 0;
 }
